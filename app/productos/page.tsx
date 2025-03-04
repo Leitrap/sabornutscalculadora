009869f365b59b1d0f79a0ec66e70582a30421d1
Leitrap <leancarm99eafc25@gmail.com>
2025-03-04 06:27:58 -0300

    Update page.tsx

--- app/productos/page.tsx
+++ app/productos/page.tsx
@@ -1,5 +1,7 @@
 "use client"
-
+// src/config.js
+const API_URL = "https://sabornutscalculadora-production.up.railway.app"; // Reemplázalo con tu URL real
+export default API_URL;
 import { useState, useEffect } from "react"
 import { ShoppingCart, Layers, History, BarChart3, LogOut, Search, Tag, Filter, ClipboardList } from "lucide-react"
 import { Button } from "@/components/ui/button"
