7de9d928c43205e614171e3f7b071b737fc68140
Leitrap <leancarm99eafc25@gmail.com>
2025-03-04 06:28:38 -0300

    Update cart-provider.tsx

--- components/cart-provider.tsx
+++ components/cart-provider.tsx
@@ -1,5 +1,7 @@
 "use client"
-
+// src/config.js
+const API_URL = "https://sabornutscalculadora-production.up.railway.app"; // Reemplázalo con tu URL real
+export default API_URL;
 import type React from "react"
 import { createContext, useContext, useState, useEffect } from "react"
 import { useToast } from "@/components/ui/use-toast"
